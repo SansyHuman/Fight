ada7a8a31df53695de55fb814cb4800e14fb99d2
mdechdee <dechdee.m@gmail.com>
2019-11-12 05:47:46 +0900

    Implement basic gun function
    - Gun Class
    - Bullet Class
    - Gun Sprite
    - Bullet Sprite
    (There are still hard coded stuffs)

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 velocity;
+
+    void Update()
+    {
+        transform.position += velocity;
+    }
+
+    public void SetVelocity(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    } 
+
+    void OnTriggerEnter(Collider collider)
+    {
+        print("oof");
+        if (collider.gameObject.name == "Stickman")
+        {
+            print("ouch!!!!");
+            collider.gameObject.GetComponent<Character>().GetDamage(1.0f);
+            Destroy(gameObject);
+        }
+    }
+}
