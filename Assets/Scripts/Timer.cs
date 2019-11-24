6cc544721e9359fb4b24c8abcdcc225d4937dd71
akmj4869 <57134776+akmj4869@users.noreply.github.com>
2019-11-25 01:11:19 +0900

    movingtile
    I just added a moving tile and a map. I think there should be a hp bar to make a toxic tile

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public static float time;
+    // for 90 seconds
+    void Start()
+    {
+        time = 90f;
+    }
+
+    void Update()
+    {
+        if (time != 0)
+        {
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                time = 0;
+            }
+        }
+        int t = Mathf.FloorToInt(time);
+        Text uiText = GetComponent<Text>();
+        uiText.text = "Time : " + t.ToString();
+    }
+}
